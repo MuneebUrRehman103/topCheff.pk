888f3d72b1c4c1588240f06b0876c76599dceaa8
IMac <imac@IMacs-iMac.local>
2018-03-31 17:13:48 +0500

    31/3/18 menuDetailsAdded dev:Muneeb

---
+++ app/src/main/java/com/example/themuneeb/myfristapp/MenuDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.themuneeb.myfristapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_details)
+    }
+}
